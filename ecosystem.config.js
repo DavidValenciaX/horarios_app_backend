fb47397784973bb33b6eae6947e65569016d5faf
DavidValenciaX <60826603+DavidValenciaX@users.noreply.github.com>
2025-06-27 13:42:43 -0500

    feat: add ecosystem configuration for PM2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+export default {
+  apps: [
+    {
+      name: 'horarios_app_backend',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
